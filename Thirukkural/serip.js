46eb53398fd2059612a0e75b9328e7538c8897d3
sivakumar8838 <142517633+sivakumar8838@users.noreply.github.com>
2023-09-30 11:47:35 +0530

    Update serip.js

--- Thirukkural/serip.js
+++ Thirukkural/serip.js
@@ -1,51 +1,60 @@
-//   to get the element the  from html
-
-let Word = document.querySelector(".Word")
-let buttonSearch=document.querySelector(".siva")
-let meaning = document.querySelector('.sk');
-// to use async function to click search button to api use meanings and part of speech
-async function searchmean() {
-    // to error handle use try and catch
-try {
-// to get the word from the user is store bu word
-    let word = Word.value;
-    // api link fetch json link
-let as=await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-let data=await as.json();
-//  console.log(data)
-    let meaningword = data[0].meanings;
-    
-    //  to create the list and list items
-
-    let list = document.createElement('ul')
-    // to use for loop meaning handle
-
-for(let partOfSpeech of meaningword ){
-    let listItem = document.createElement('li');
-    // to append the inner html listItem
-
-    listItem.innerHTML = ` <b class="sa"><hr>Part Of Speech:${partOfSpeech.partOfSpeech}</b>`;
-    // to create the order list is sublist
-
-    let subList = document.createElement('ol');
-    
-for (let definition of partOfSpeech.definitions) {
-        let subListItem = document.createElement('li');
-        subListItem.innerHTML = `${definition.definition}`;
-        subList.appendChild(subListItem);
-    }
-
-    // append the sublist to the list item
-    listItem.appendChild(subList);
-
-    // append the list items to the main list
-    list.appendChild(listItem);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        h1{
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            margin: 5%;
+            text-shadow: 2px 1px rgb(9, 9, 245);
+            
+        }
+        .red{
+            color: rgb(21, 216, 14);
+        }
+        .sk{
+            border: solid;
+            border-radius: 15px;
+            box-shadow: 4px 4px rgb(40, 41, 36);
+    height: 100%;
+    margin-left: 20%;
+    margin-right: 20%;
+    padding-left: 5%;
+    padding-top: 4%;
+        }
+        button{
+    border-radius: 8px;
+    background-color: rgb(250, 105, 8);
+    color: aliceblue;
 }
-
-meaning.innerHTML=``;
-meaning.appendChild(list);
+button:hover{
+    background-color: rgb(57, 57, 65);
 }
-catch (error) {
-console.log('error fetching the meaning', error);
+body{
+    background-color: rgb(192, 229, 149);
 }
+.tamilKuralExp{
+   color: rgb(227, 21, 97);
 }
+.as{
+color: rgb(14, 14, 254);
+box-shadow: 2px 2px rgb(242, 20, 28);
+}
+    </style>
+</head>
+<body>
+    <div class="contaniner">
+        <h1>THIRUKURAL BOOK</h1>
+        <hr class="as">
+     <div class="sk">
+  
+      </div>
+    <script src="./serip.js"></script>
+</body>
+</html>

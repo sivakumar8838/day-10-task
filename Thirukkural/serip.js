d334a643d0c16172558c83d567088114df79dd3f
sivakumar8838 <142517633+sivakumar8838@users.noreply.github.com>
2023-09-30 11:49:04 +0530

    Update serip.js

--- Thirukkural/serip.js
+++ Thirukkural/serip.js
@@ -1,60 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        h1{
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            margin: 5%;
-            text-shadow: 2px 1px rgb(9, 9, 245);
-            
-        }
-        .red{
-            color: rgb(21, 216, 14);
-        }
-        .sk{
-            border: solid;
-            border-radius: 15px;
-            box-shadow: 4px 4px rgb(40, 41, 36);
-    height: 100%;
-    margin-left: 20%;
-    margin-right: 20%;
-    padding-left: 5%;
-    padding-top: 4%;
-        }
-        button{
-    border-radius: 8px;
-    background-color: rgb(250, 105, 8);
-    color: aliceblue;
-}
-button:hover{
-    background-color: rgb(57, 57, 65);
-}
-body{
-    background-color: rgb(192, 229, 149);
-}
-.tamilKuralExp{
-   color: rgb(227, 21, 97);
-}
-.as{
-color: rgb(14, 14, 254);
-box-shadow: 2px 2px rgb(242, 20, 28);
-}
-    </style>
-</head>
-<body>
-    <div class="contaniner">
-        <h1>THIRUKURAL BOOK</h1>
-        <hr class="as">
-     <div class="sk">
-  
-      </div>
-    <script src="./serip.js"></script>
-</body>
-</html>
+      // create the function for specific thirukural at this explain and english translate to button click 
+      function explainkural(a){
+         // n is the thirukural number 
+         let siva = document.querySelectorAll(".full")[a - 1]; // Select the corresponding explanation div
+         siva.innerHTML = ""; // Clear previous explanations if any
+      fetch(`https://api-thirukkural.vercel.app/api?num=${a}`)
+      .then(sa=>sa.json())
+      .then(data=>{
+         //  to get the information form the thirukural api
+         let title=data.sect_tam;
+         let chapter=data.chap_tam
+         let exp=data.tam_exp
+         let engtitle=data.sect_eng;
+         let engChap=data.chap_eng;
+         let engKural1=data.eng;
+         let engExp=data.eng_exp;
+         //  to append the infromation to create the div tag
+         let tamil=document.createElement('div')
+         tamil.className="tamilKuralExp"
+         tamil.innerHTML=` <hr>  <h4 class="red">${title}</h4>
+                             <h6>${chapter}</h6>
+                             <p>${exp}</p>
+                             <h4 class="red">${engtitle}</h4>
+                             <h6>${engChap}</h6>
+                             <p>${engKural1}</p>
+                             <p>${engExp}</p><hr>`
+         siva.appendChild(tamil)                    
+ 
+      })
+     }
+ 
+     // Create an async function to fetch and display Thirukural  in order
+ 
+     async function Thirukural() {
+         for (let i = 1; i <= 1330; i++) {
+             // Use await to fetch each sequentially
+           try{
+             let response = await fetch(`https://api-thirukkural.vercel.app/api?num=${i}`);
+           
+             let data = await response.json();
+ 
+             // Create a new paragraph element for each verse
+             let kurals = document.createElement('p');
+     
+             kurals.innerHTML = `
+                     <p>${i}: ${data.line1}</p>
+                     <p class="line2">${data.line2}</p>
+                     <button class="kuralExplain" onclick="explainkural(${i})">Explain</button><hr>
+                     <div class="full"></div>
+             `;
+ 
+             // Append the order to the outblock container
+             let as = document.querySelector(".sk");
+             as.appendChild(kurals);
+           }
+         //   to error handle for the thirukural use catch
+            catch(error){
+             console.log('error for thirukral',error)
+            }
+         }
+     }
+ 
+     // Call the async function to display in order when the page loads
+    Thirukural()
